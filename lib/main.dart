f572a49b08003f48bc62ab332bdae29add620ca3
Yassin522 <yasinalmhdi8@gmail.com>
2022-09-14 21:16:18 +0300

    mnor

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  //منوررررررررررررررررررررررررررررررررررررررررررررر
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
