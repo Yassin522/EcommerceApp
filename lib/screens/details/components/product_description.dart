d288937df1af287b52f979c9ef0d2ce34d5101d0
MahmoudKok <mahmoudkoka39@gmail.com>
2022-09-25 17:50:51 +0300

    detail

--- lib/screens/details/components/product_description.dart
+++ lib/screens/details/components/product_description.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/screens/sign_in/Models/global_user_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ecommerce/models/Product.dart';
@@ -20,6 +21,35 @@ class ProductDescription extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        Visibility(
+            visible: GlobalUserInfo.user!.role_id == '1' ? false : true,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(30)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  DetaiActionButton(
+                    onPress: () {},
+                    icon: Icons.add,
+                  ),
+                  SizedBox(width: getProportionateScreenWidth(10)),
+                  DetaiActionButton(
+                    onPress: () {},
+                    icon: Icons.edit,
+                  ),
+                  SizedBox(width: getProportionateScreenWidth(10)),
+                  DetaiActionButton(
+                    onPress: () {},
+                    icon: Icons.delete,
+                  ),
+                  SizedBox(width: getProportionateScreenWidth(10)),
+                ],
+              ),
+            )),
+        SizedBox(
+          height: getProportionateScreenHeight(12),
+        ),
         Padding(
           padding:
               EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
@@ -34,8 +64,7 @@ class ProductDescription extends StatelessWidget {
             padding: EdgeInsets.all(getProportionateScreenWidth(15)),
             width: getProportionateScreenWidth(64),
             decoration: BoxDecoration(
-              color:
-                  Color(0xFFFFE6E6) ,
+              color: Color(0xFFFFE6E6),
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(20),
                 bottomLeft: Radius.circular(20),
@@ -43,8 +72,7 @@ class ProductDescription extends StatelessWidget {
             ),
             child: SvgPicture.asset(
               "assets/icons/Heart Icon_2.svg",
-              color:
-                   Color(0xFFFF4848) ,
+              color: Color(0xFFFF4848),
               height: getProportionateScreenWidth(16),
             ),
           ),
@@ -84,6 +112,23 @@ class ProductDescription extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class DetaiActionButton extends StatelessWidget {
+  DetaiActionButton({
+    Key? key,
+    this.icon,
+    this.onPress,
+  }) : super(key: key);
+  final onPress;
+  final icon;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPress,
+      icon: Icon(icon),
     );
   }
 }
